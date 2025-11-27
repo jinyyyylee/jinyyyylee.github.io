3635542b3231e34eb1cc75f3a90cdecdbc0c1fba
jylee <jinyyyy_lee@naver.com>
2025-11-27 14:38:00 +0900

    Updates

--- _next/static/VW_teJF0xFYGpX1lGl7Iv/_buildManifest.js
+++ _next/static/VW_teJF0xFYGpX1lGl7Iv/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/jy-dev.github.io//_next/:path+",
+        "destination": "/jy-dev.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
